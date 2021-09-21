e57c44de56373e3a3d34e60e029c35f2fed36fab
OdiSeU <secret36565@gmail.com>
2021-09-21 16:37:29 +0900

    210921
    17681 done

--- Programmers_JS/17681.js
+++ Programmers_JS/17681.js
@@ -1,8 +1,8 @@
 function solution(n, arr1, arr2) {
     let answer = [], code;
     for(let i=0; i<n; i++) {
-        code = (arr1[i] | arr2[i]).toString(2);
-        answer.push(code.replaceAll('1','#').replaceAll('0',' '));
+        code = (arr1[i] | arr2[i]).toString(2).padStart(n, 0);
+        answer.push(code.replace(/1/g,'#').replace(/0/g,' '));
     }
     return answer;
 }
