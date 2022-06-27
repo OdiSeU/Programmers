0a7109ab93a493fb63e0fc1f991f63a26e429d9b
OdiseU <secret36565@gmail.com>
2022-06-27 13:20:00 +0900

    220627
    42746 done

--- Programmers_JS/42746.js
+++ Programmers_JS/42746.js
@@ -1,10 +1,7 @@
 function solution(numbers) {
     let answer = numbers.sort((a,b)=>{
         a += ''; b += '';
-        let [x, y] = [a.padEnd(4, a[0]), b.padEnd(4, b[0])];
-        // console.log(a, b);
-        // console.log(x, y, x==y, x == y ? b.length - a.length : x < y ? 1 : -1);
-        return x == y ? a.length - b.length : x < y ? 1 : -1;
+        return ((b+a)|0) - ((a+b)|0);
     }).join('');
     return answer[0] == '0' ? '0' : answer;
 }
