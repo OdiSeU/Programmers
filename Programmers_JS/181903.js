48a41468e9005ae4696cfce3d6ee68f640d4e570
OdiseU <binarycastle98@gmail.com>
2024-08-19 17:32:33 +0900

    240503

--- Programmers_JS/181903.js
+++ Programmers_JS/181903.js
@@ -1,5 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181903
 
 function solution(q, r, code) {
-    return [...code].filter((_, i) => i % q === r).join("");
+  return [...code].filter((_, i) => i % q === r).join("");
 }
