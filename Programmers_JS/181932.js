c78027119b518433fbd1fe28775cfb4b03da7cb0
OdiseU <binarycastle98@gmail.com>
2024-08-19 16:35:34 +0900

    240819

--- Programmers_JS/181932.js
+++ Programmers_JS/181932.js
@@ -1,6 +1,37 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181932
+
+/**
+ * 풀이 1
+ * @param {string} code
+ * @returns
+ */
 function solution(code) {
-    return code
-        .split("1")
-        .map((v, idx) => [...v].filter((_, i) => idx % 2 === i % 2).join(""))
-        .join("");
+    let mode = 0;
+    let ret = [];
+
+    for (let i = 0; i < code.length; i++) {
+        if (code[i] === '1') {
+            mode = mode ? 0 : 1;
+            continue;
+        }
+        if (i % 2 === mode) {
+            ret.push(code[i]);
+        }
+    }
+
+    return ret.length ? ret.join('') : 'EMPTY';
+}
+
+/**
+ * 풀이 2
+ * @param {string} code
+ * @returns
+ */
+function solution(code) {
+    return (
+        [...code]
+            .filter((v) => v !== '1')
+            .filter((_, i) => !(i % 2))
+            .join('') || 'EMPTY'
+    );
 }
