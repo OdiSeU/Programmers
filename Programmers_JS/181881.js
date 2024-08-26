7c84cf1c60c150c29571fd696bdaab370769b3f5
OdiseU <binarycastle98@gmail.com>
2024-08-26 18:40:00 +0900

    240826

---
+++ Programmers_JS/181881.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    let answer = 0;
+    const getX = (n) => {
+        if (n < 50 && n % 2 === 0) return 0;
+        if (n > 50 && n % 2 === 1) return 0;
+        if (n === 1) return 5;
+        if (n < 6) return 4;
+        if (n < 12) return 3;
+        if (n < 24) return 2;
+        if (n < 50) return 1;
+        if (n === 100) return 3;
+        if (n % 4 === 0) return 1;
+        return 2;
+    };
+
+    for (const n of arr) {
+        answer = Math.max(answer, getX(n));
+    }
+
+    return answer;
+}
